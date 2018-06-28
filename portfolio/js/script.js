b192ff8bf908f3baca9bb7528114140af145030a
Ajkula91 <nenadmilunov91@gmail.com>
2018-06-29 00:21:08 +0200

    started skills section - pie chart

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,17 +1,36 @@
 $(document).ready(function() {
-
+    //superslides code
     $('#slides').superslides({
         animation: 'fade',
         play: 5000,
         pagination: false
     });
 
+    //typed code
     var typed = new Typed(".typed", {
         strings: ["Web Developer", "Web Designer", "Software Programmer"],
         typeSpeed: 90,
         loop: true,
         startDelay: 1200,
         showCursor: false
+    });
+
+    //owl carousel code
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
     });
 
 });
