25bff6d24118fd8bacdf6282279c4dc83ae18fae
Ajkula91 <nenadmilunov91@gmail.com>
2018-07-01 15:37:49 +0200

    Done with stats section

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -36,6 +36,8 @@ $(document).ready(function() {
     });
 
     var skillsTopOffset = $(".skillsSection").offset().top;
+    var statsTopOffset = $(".statsSection").offset().top;
+    var countUpFinished = false;
     
     $(window).scroll(function() {
         if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
@@ -52,6 +54,19 @@ $(document).ready(function() {
             }
         });
         }
+
+        if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+            //$(".counter").countup(200);
+            $(".counter").each(function() {
+                var element = $(this);
+                var endVal = parseInt(element.text());
+
+                element.countup(endVal);
+            });
+
+            countUpFinished = true;
+        }
+
     });
 
 });
