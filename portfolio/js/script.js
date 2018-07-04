361bc5f3c48f4062a15c2655ac2eb4dac7ca2f77
Ajkula91 <nenadmilunov91@gmail.com>
2018-07-04 11:25:09 +0200

    something

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,147 +1,169 @@
-//loader
 $(window).on("load", function() {
-   $(".loader .inner").fadeOut(500, function() {
-        $(".loader").fadeOut(1000);
-   }); 
-});
+
+	$(".loader .inner").fadeOut(500, function() {
+		$(".loader").fadeOut(750);
+	});
+
+})
+
+
+
 
 $(document).ready(function() {
-    //superslides code
-    $('#slides').superslides({
-        animation: 'fade',
-        play: 5000,
-        pagination: false
-    });
 
-    //typed code
-    var typed = new Typed(".typed", {
-        strings: ["Web Developer", "Web Designer", "Software Programmer"],
-        typeSpeed: 90,
-        loop: true,
-        startDelay: 1200,
-        showCursor: false
-    });
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false
+	});
 
-    //owl carousel code
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        items: 4,
-        responsive:{
-            0:{
-                items:1
-            },
-            480:{
-                items:2
-            },
-            768:{
-                items:3
-            },
-            938:{
-                items:4
-            }
-        }
-    });
+	var typed = new Typed(".typed", {
+		strings: ["Software Engineer.", "Web Developer.", "Student."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
 
-    var skillsTopOffset = $(".skillsSection").offset().top;
-    var statsTopOffset = $(".statsSection").offset().top;
-    var countUpFinished = false;
-    
-    $(window).scroll(function() {
-        if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-                //easy pie chart
-         $('.chart').easyPieChart({
-            easing: 'easeInOut',
-            barColor: '#fff',
-            trackColor: false,
-            scaleColor: false,
-            lineWidth: 4,
-            size: 152,
-            onStep: function(from, to, percent) {
-                $(this.el).find('.percent').text(Math.round(percent));
-            }
-        });
-        }
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+	});
 
-        if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
-            //$(".counter").countup(200);
-            $(".counter").each(function() {
-                var element = $(this);
-                var endVal = parseInt(element.text());
 
-                element.countup(endVal);
-            });
+	
 
-            countUpFinished = true;
-        }
 
-    });
+	var skillsTopOffset = $(".skillsSection").offset().top;
+	var statsTopOffset = $(".statsSection").offset().top;
+	var countUpFinished = false;
+	$(window).scroll(function() {
 
-    //fancy js
-    $("[data-fancybox]").fancybox({
-        arrows: true,
-        animationEffect: "zoom",
-        clickContent: false
-    });
+		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
 
-    //call isotope
-    $(".items").isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 1500,
-            easing: 'linear',
-            queue: false
-        }
-    });
+			$('.chart').easyPieChart({
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		    });
 
-    $("#filters a").click(function() {
 
-        $("#filters .current").removeClass("current");
-        $(this).addClass("current");
+		}
 
-        var selector = $(this).attr("data-filter");
 
-        $(".items").isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 1500,
-                easing: 'linear',
-                queue: false
-            }
-        });
+		if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+			$(".counter").each(function() {
+				var element = $(this);
+				var endVal = parseInt(element.text());
 
-        return false;
+				element.countup(endVal);
+			})
 
-    });
+			countUpFinished = true;
 
-    //smooth scrooling navigation (no plugin)
-    $("#navigation li a").click(function(e) {
-        e.preventDefault();
+		}
 
-        var targetElement = $(this).attr("href");
-        var targetPosition = $(targetElement).offset().top;
-        $("html, body").animate({scrollTop: targetPosition - 35}, "slow");
-    });
 
-    const nav = $("#navigation");
-    const navTop = nav.offset().top;
+	});
 
-    $(window).on("scroll", stickyNavigation);
 
-    function stickyNavigation() {
-        var body = $("body");
+	$("[data-fancybox]").fancybox();
 
-        if($(window).scrollTop() >= navTop) {
-            body.css("padding-top", nav.outerHeight() + "px");
-            body.addClass("fixedNav");
-        }
-        else {
-            body.css("padding-top", 0);
-            body.removeClass("fixedNav");
-        }
 
-    }
+	$(".items").isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 9500,
+			easing: 'linear',
+			queue: false
+		}
+	});
+
+	$("#filters a").click(function() {
+
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		var selector = $(this).attr("data-filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 1500,
+				easing: 'linear',
+				queue: false
+			}
+		});
+
+		return false;
+	});
+
+
+
+	$("#navigation li a").click(function(e) {
+		e.preventDefault();
+
+		var targetElement = $(this).attr("href");
+		var targetPosition = $(targetElement).offset().top;
+		$("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+
+	});
+
+
+
+
+	const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+
+		var body = $("body");
+
+		if($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav");
+		}
+		else {
+			body.css("padding-top", 0);
+			body.removeClass("fixedNav");
+		}
+
+
+
+
+	}
 
 });
+
+
+
+
+
+
+
+
 
 
 
